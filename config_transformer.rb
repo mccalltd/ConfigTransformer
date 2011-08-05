10d7a10759bccab66b7ede0f7cbe12c34d4f98d2
Tim McCall <mccalltd@gmail.com>
2011-08-05 03:03:00 -0600

    added env specific config transformations. now ready for the real world.

--- config_transformer.rb
+++ config_transformer.rb
@@ -15,20 +15,23 @@ require 'nokogiri'
 
 class ConfigTransformer
 
-  def initialize(master_config_path, target_config_path)
+  def initialize(master_config_path, target_config_path, env = "default")
     raise ArgumentError, "master_config_path is required" if master_config_path.nil?
     raise ArgumentError, "target_config_path is required" if target_config_path.nil?
 
     @master_config_path = master_config_path
     @target_config_path = target_config_path
+    @env = env
   end
   
   def execute    
     @master_config = open_xml_doc @master_config_path
     @target_config = open_xml_doc @target_config_path		
 
-    baseline_target_with @master_config
-    write_xml_to_target
+    transform_target_to_baseline_with @master_config
+    transform_target_to_environment_with @master_config if @env != "default"
+    
+    write_target
   end
 	
 private
@@ -39,39 +42,40 @@ private
     doc
   end
 
-  def baseline_target_with(node)
+  def transform_target_to_baseline_with(node)
     node.children.each do |n|
-      # skip environment specific elements, we're baselining with defaults
+      # skip environment specific elements or processed elements
       next if (n.attr("env") || "default") != "default"
-      
-      ancestor_names = n.ancestors.map {|a| a.name}
-      case
-      when ancestor_names.include?("connectionStrings") && n.name == "add" 
-        update_connection_string_with n 
-      when ancestor_names.include?("appSettings") && n.name == "add" 
-        update_app_setting_with n
-      when node.attr("env") 
-        update_element_with n 
-        next
-      end
-      
-      baseline_target_with n
+      next if transform_target_with n
+      transform_target_to_baseline_with n
     end
   end
 
-  def update_connection_string_with(node)
-    add_to_target node, "[@name='#{node.attr("name")}']"
+  def transform_target_to_environment_with(node)    
+    node.children.each do |n|
+      # skip anything not relevent to the current environment or processed elements
+      unless n.attr("env") != @env
+        next if transform_target_with n
+      end
+      transform_target_to_environment_with n
+    end
   end
 
-  def update_app_setting_with(node)
-    add_to_target node, "[@key='#{node.attr("key")}']"
+  def transform_target_with(node)
+    ancestor_names = node.ancestors.map {|a| a.name}
+    case
+    when ancestor_names.include?("connectionStrings") && node.name == "add" 
+      add_to_target node, "[@name='#{node.attr("name")}']"
+    when ancestor_names.include?("appSettings") && node.name == "add" 
+      add_to_target node, "[@key='#{node.attr("key")}']"
+    when node.attr("env") 
+      add_to_target node
+    else 
+      return false # did not process node 
+    end
+    true # processed node
   end
-
-  def update_element_with(node)		
-    add_to_target node
-    @target_config.at_xpath(node.path).remove_attribute "env"	
-  end
-
+  
   def add_to_target(node, attr_selector = nil)
     # not sure why the given node has an index in its path -- need to strip it
     target_node = @target_config.at_xpath "#{node.path.gsub(/\[.*?\]$/, "")}#{attr_selector}"
@@ -81,6 +85,8 @@ private
     else
       target_node.replace node
     end
+    # remove any "env" attribute that might be on this node
+    @target_config.at_xpath(node.path).remove_attribute "env"	
   end
 
   def ensure_ancestors_exist(node)
@@ -94,11 +100,11 @@ private
     end
   end
 
-  def write_xml_to_target
+  def write_target
     f = File.open @target_config_path, "w"
     @target_config.write_xml_to(f)
     f.close
   end
 end
 
-ConfigTransformer.new("master.config", "web.config").execute
+ConfigTransformer.new("master.config", "web.config", "ci").execute
